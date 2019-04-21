5ff84bb41e5b91cf47ac52a36625b4f34d293bb9
tazeen <tazeen.fatima@progstream.com>
2019-04-22 03:09:39 +0500

    add navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,17 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/signup/signup.component';
+import { NavigationComponent } from './components/navigation/navigation.component'
 
 // Routes
-import { appRoutes } from './routes'
+import { appRoutes } from './routes';
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     SignUpComponent,
-    LoginComponent
+    LoginComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
