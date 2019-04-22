74b4e81f00f21561af7176c9ddf2b5daf1ebaa6f
tazeen <tazeen.fatima@progstream.com>
2019-04-23 01:21:02 +0500

    changes in top navigation bar

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn = false;
+  showList = false;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  logoutUser(){
+    //logout
+  }
+  routeTo(path){
+    this.router.navigate([`${path}`]);
   }
 
 }
